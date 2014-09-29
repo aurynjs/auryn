d4d55ff061fb386430c8ab95c660e85a7b1d9176
David Corticchiato <david.corticchiato@iscool-e.com>
2014-09-29 17:15:01 +0200

    create ios framework

---
+++ src/ios/auryn/auryn/auryn.m
@@ -0,0 +1,13 @@
+//
+//  auryn.m
+//  auryn
+//
+//  Created by David Corticchiato on 29/09/2014.
+//  Copyright (c) 2014 Phantasien. All rights reserved.
+//
+
+#import "auryn.h"
+
+@implementation auryn
+
+@end
